b1c5b943c8354368651a7dc71e44b2972580916f
Ivy Lin <a0981792089@gmail.com>
2019-10-28 23:54:55 +0800

    建立子樣板 編輯index.balde.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,11 +1,8 @@
-<html>
-<head>
-    <title>@yield('title')|MyBlog</title>
-</head>
-<body>
-    <div class="container">
-    @yield('content')
-    </div>
-</body>
-</html>
+@extends('layouts.master')
+@section('title','Home')
+@section('content')
+    <h1>Home</h1>
+    <p>This is my body content.</p>
+@endsection
+
 
